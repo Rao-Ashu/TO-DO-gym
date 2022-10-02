809dedcc6072777bc7885053f77d9fb90090f959
Rao-Ashu <mit2021066@iiita.ac.in>
2022-10-02 11:54:54 +0530

    add set count and reset button with thier functionalities

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var items = [];
+var setCount = [];
 app.get('/', function(req,res){
   var day = new Date();
   var options = {
@@ -17,14 +18,33 @@ app.get('/', function(req,res){
 
   var today = day.toLocaleDateString("en-US",options);
 
-  res.render('list', {presentDay: today, itemList:items});
+  res.render('list', {presentDay: today, itemList:items, setList:setCount});
 });
 
 app.post('/',function(req,res){
+  console.log(req.body);
+  var newItemSetCount = req.body.setNo;
   var newItemAdded = req.body.addNew;
   if(newItemAdded.length !=0){
     items.push(newItemAdded);
   }
+  if(newItemSetCount >0){
+    setCount.push(newItemSetCount);
+  }else if (newItemSetCount <=0) {
+    setCount.push('0');
+  }
+
+
+  // RESETbtn
+  if(req.body.reset === ''){
+    setCount = [];
+    items = [];
+  }
+
+  // DELETEbtn
+  // if(req.body.delete===''){
+  //
+  // }
 
   res.redirect('/');
 });
