81d8565118399655a8c3e9bee9de1c0c061d8f1d
Rao-Ashu <mit2021066@iiita.ac.in>
2022-10-03 09:02:18 +0530

    add the functionality of delete button,enable user to delete individual entry.

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require ("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 var items = [];
 var setCount = [];
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
   var day = new Date();
   var options = {
     day: "numeric",
@@ -16,35 +18,51 @@ app.get('/', function(req,res){
     weekday: "long"
   };
 
-  var today = day.toLocaleDateString("en-US",options);
+  var today = day.toLocaleDateString("en-US", options);
 
-  res.render('list', {presentDay: today, itemList:items, setList:setCount});
+  res.render('list', {
+    presentDay: today,
+    itemList: items,
+    setList: setCount
+  });
 });
 
-app.post('/',function(req,res){
-  console.log(req.body);
+app.post('/', function(req, res) {
+
+  // new_items
   var newItemSetCount = req.body.setNo;
   var newItemAdded = req.body.addNew;
-  if(newItemAdded.length !=0){
+
+  if (newItemAdded.length != 0) {
     items.push(newItemAdded);
   }
-  if(newItemSetCount >0){
+
+
+  if (newItemSetCount > 0) {
     setCount.push(newItemSetCount);
-  }else if (newItemSetCount <=0) {
+  } else if (newItemSetCount <= 0) {
     setCount.push('0');
   }
 
 
   // RESETbtn
-  if(req.body.reset === ''){
+  if (req.body.resetbtn === '') {
     setCount = [];
     items = [];
   }
 
   // DELETEbtn
-  // if(req.body.delete===''){
-  //
-  // }
+  if (req.body.delete) {
+    // var a = document.getElementById("button");
+    // console.log(a);
+    // console.log(req.body);
+
+    const index = items.indexOf(req.body.delete);
+    if (index > -1) { // only splice array when item is found
+      items.splice(index, 1);
+      setCount.splice(index, 1);
+    }
+  }
 
   res.redirect('/');
 });
